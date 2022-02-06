b985eacc2b59cd4d24d3fb81e4de8ebf3260aff6
LunaAnima <99130864+LunaAnima@users.noreply.github.com>
2022-02-06 15:54:42 +0100

    paths updated for local page

--- scripts/require-init.js
+++ scripts/require-init.js
@@ -3,10 +3,10 @@ require.config({
     paths: {
         vue: "https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue",
         jquery: "https://code.jquery.com/jquery-3.6.0.min",
-        fragen: "/scripts/fragen",
-        descriptions: "/scripts/descriptions",
-        score: "/scripts/score",
-        game: "/scripts/game",
-        darstellung: "/scripts/darstellung",
+        fragen: "/draonia-faction-quiz/scripts/fragen",
+        descriptions: "/draonia-faction-quiz/scripts/descriptions",
+        score: "/draonia-faction-quiz/scripts/score",
+        game: "/draonia-faction-quiz/scripts/game",
+        darstellung: "/draonia-faction-quiz/scripts/darstellung",
     }
 });
