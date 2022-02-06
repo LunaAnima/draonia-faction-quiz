5a5414761bf5d2e22ed5162873e1bdfc8600558b
LunaAnima <99130864+LunaAnima@users.noreply.github.com>
2022-02-06 15:57:06 +0100

    Update quiz.js

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,4 +1,4 @@
-require(["/scripts/require-init.js"], function (config) {
+require(["/draonia-faction-quiz/scripts/require-init.js"], function (config) {
     require(['fragen', 'darstellung', 'game', 'descriptions'],
                   function( fragen, darstellung, game, descriptions) {
 
